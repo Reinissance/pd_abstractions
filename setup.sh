5b34e53a800fb63d597a256206c5ec439e3937cb
Reinissance <reinissance@gmail.com>
2025-01-14 10:42:55 +0100

    setup sparse-checkedout

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+# Variables
+REPO_DIR="list-abs"
+SUBMODULE_PATH="/LICENSE.txt
+/list-sort.pd
+/list-sort-help.pd
+/list-unique.pd
+/list-unique-help.pd
+/list-compare.pd
+/list-compare-help.pd
+/compare-any.pd
+/compare-any-help.pd
+/list-len.pd
+/list-len-help.pd
+/list-dripslow.pd
+/list-dripslow-help.pd
+/list-find.pd
+/list-find-help.pd
+/list-drip.pd
+/list-drip-help.pd
+/list-rdrip.pd
+/list-rdrip-help.pd"
+
+# Navigate to the repository directory
+cd $REPO_DIR
+
+# Initialize sparse-checkout and set submodule path
+git sparse-checkout init --cone
+git sparse-checkout set $SUBMODULE_PATH
